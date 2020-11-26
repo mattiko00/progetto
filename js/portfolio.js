f9e11b4f28c5813b9ad8b06b9d35480df31714a1
Mattia <74598686+mattiko00@users.noreply.github.com>
2020-11-26 15:19:51 +0100

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,23 @@
+// Doughnut
+var ctxD = document.getElementById("doughnutChart").getContext('2d');
+var myLineChart = new Chart(ctxD, {
+  type: 'doughnut',
+  data: {
+    labels: ["Chrome", "Opera", "Firefox", "Navigator", "Safari"],
+    datasets: [{
+      data: [300, 50, 100, 40, 120],
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+    }]
+  },
+  options: {
+    responsive: true,
+    legend: {
+      position: 'right',
+      align: 'center',
+      labels: {
+        padding: 20
+      }
+    }
+  }
+});
